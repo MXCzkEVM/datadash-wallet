086ee0dbc2f97a9056307d165f9f39fcde84a5a4
Sheen <hx.sheen@gmail.com>
2023-05-27 23:48:27 +0200

    test gpt

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:datadashwallet/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    // await tester.pumpWidget(const MyApp());
+void mai
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -21,7 +18,7 @@ void main() {
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumdfdp();
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
