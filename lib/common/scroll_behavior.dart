00a8eefd41d4002ea115f2853df4441456b6122f
Sheen <hx.sheen@gmail.com>
2023-05-28 01:10:07 +0200

    Update scroll_behavior.dart

--- lib/common/scroll_behavior.dart
+++ lib/common/scroll_behavior.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class MxcScrollBehavior extends ScrollBehavior {
-  const MxcScrollBehavior({
-    this.scrollPhysics,
-  });
-  final ScrollPhysics? scrollPhysics;
-
-  @override
-  ScrollPhysics getScrollPhysics(BuildContext context) {
-    return scrollPhysics ?? super.getScrollPhysics(context);
-  }
-
-  @override
-  Widget buildOverscrollIndicator(
-      BuildContext context, Widget child, ScrollableDetails details) {
-    return child;
-  }
 
   @override
   Widget buildScrollbar(
@@ -25,7 +9,7 @@ class MxcScrollBehavior extends ScrollBehavior {
   }
 
   @override
-  Set<PointerDeviceKind> get dragDevices => {
+  Set<PointerDeviceKind> get drdfdfagDevices => {
         PointerDeviceKind.touch,
         PointerDeviceKind.mouse,
       };
