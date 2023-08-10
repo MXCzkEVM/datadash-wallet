77723051c7f330ad59a3595abf11f869b0bbfd5e
Christian <christian@mxc.org>
2023-08-10 14:14:33 +0200

    updating config

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -9,25 +9,26 @@ set -e
 set -x
 
 cd ..
-git clone -b beta https://github.com/flutter/flutter.git
+git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 
-flutter channel stable
-flutter doctor
+flutter clean
+
+# accepting all licenses
+yes | flutter doctor --android-licenses
+
+echo "Installed flutter to `pwd`/flutter"
 
 echo "Installed flutter to `pwd`/flutter"
 
 # build APK
-# if you get "Execution failed for task ':app:lintVitalRelease'." error, uncomment next two lines
-# flutter build apk --debug
-# flutter build apk --profile
-flutter build apk --release
+flutter build apk --flavor prod
 
 # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
-flutter build appbundle --release --build-number $APPCENTER_BUILD_ID
+flutter build appbundle --flavor play
 
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/prod/release/app-prod-release.apk $_
 
 # copy the AAB where AppCenter will find it
-mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
+mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/playRelease/app-play-release.aab $_
